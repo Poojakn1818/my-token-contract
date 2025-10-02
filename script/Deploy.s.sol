6c6b91c3ef2897fa7d3b2f39e28f4d8608016c1f
Pooja K N <poojakn1818@gmail.com>
2025-10-02 21:41:34 +0200

    initial commit: ERC20 token deployment on Sepolia

---
+++ script/Deploy.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/SimpleToken.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerPrivateKey);
+        
+        SimpleToken token = new SimpleToken();
+        
+        console.log("SimpleToken deployed at:", address(token));
+        
+        vm.stopBroadcast();
+    }
+}
